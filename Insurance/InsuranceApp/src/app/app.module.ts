899dc96065089fb239e7d90b6f89c3f2be5910c4
Pushparaj <raju.vellayil@gmail.com>
2022-03-11 18:01:34 +0530

    Added Premium component

--- Insurance/InsuranceApp/src/app/app.module.ts
+++ Insurance/InsuranceApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PremiumComponent } from './Premium/premium/premium.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PremiumComponent
   ],
   imports: [
     BrowserModule,
