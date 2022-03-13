1eab77bebda9b62747fd29b719df792c2b7f298e
Pushparaj <raju.vellayil@gmail.com>
2022-03-13 19:41:58 +0530

    Added High level  nunit test cases

---
+++ Api/Insurance.Api/Insurance.Api.Tests/PremiumControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using Insurance.Api.Controllers;
+using Insurance.Models;
+using Insurance.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Threading;
+
+namespace Insurance.Api.Tests
+{
+    public class PremiumControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void CalculateMonthlyPremiumOkResponse()
+        {
+            //Arrange
+            var mockRepository = new Mock<IPremiumService>();
+            var controller = new PremiumsController(mockRepository.Object);
+            // Act
+            var actionResult = controller.CalculateMonthlyPremium(new CustomerModel(), new CancellationToken());
+            //Assert
+            Assert.IsInstanceOf<OkObjectResult>(actionResult);
+
+        }
+    }
+}
---
+++ Api/Insurance.Api/Insurance.Services.Tests/PremiumServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using Insurance.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Insurance.Services.Tests
+{
+    [TestFixture]
+    public class PremiumServiceTest
+    {
+        Mock<IRatingService> _mockRatingSerivce;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRatingSerivce = new Mock<IRatingService>(MockBehavior.Strict);
+
+        }
+
+        [Test]
+        public void UT_Validate_MontlyPremium_HappyPath()
+        {
+            //Arrange
+            int age = 38;
+            string occupation = "Professional";
+            double premium = 1000;
+            _mockRatingSerivce.Setup(p => p.GetRating(occupation)).Returns(100);
+            var premiumService = new PremiumService(_mockRatingSerivce.Object);
+            //Act
+            var totalPremium = premiumService.Calculate(age, occupation, premium);
+            //Assert
+            Assert.NotZero(totalPremium.TotalMonthlyPremium);
+        }
+        [Test]
+        public void UT_Validate_MontlyPremium_Check_ArgumetException()
+        {
+            //Arrange
+            int age = 0;
+            string occupation = "Professional";
+            double premium = 1000;
+            _mockRatingSerivce.Setup(p => p.GetRating(occupation)).Returns(1000);
+            var premiumService = new PremiumService(_mockRatingSerivce.Object);
+            //Act Assert
+            Assert.That(() => premiumService.Calculate(age, occupation, premium), Throws.TypeOf<ArgumentException>());
+
+        }
+        [Test]
+        public void UT_Validate_MontlyPremium_Check_ArgumetOutOfRangeException()
+        {
+            //Arrange
+            int age = 38;
+            string occupation = "Test";
+            double premium = 1000;
+            _mockRatingSerivce.Setup(p => p.GetRating(occupation)).Returns(0);
+            var premiumService = new PremiumService(_mockRatingSerivce.Object);
+            //Act Assert
+            Assert.That(() => premiumService.Calculate(age, occupation, premium), Throws.TypeOf<ArgumentOutOfRangeException>());
+
+        }
+    }
+}
